5d86e289a136a1115362cb6cb710891b35860692
NguyenTrongTot <164918193+TrongtotDHFPT@users.noreply.github.com>
2025-05-27 16:51:39 +0700

    Push userDTO

---
+++ backend/HealthcareSystem.Application/DTOs/UserDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthcareSystem.Application.DTOs
+{
+    public class UserDTO
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateOnly? DoB { get; set; }
+        public string? Gender { get; set; }
+        public string? Address { get; set; }
+        public string? Avatar { get; set; }
+
+        // Password chỉ cần trong ngữ cảnh đăng ký, không trả về cho client
+        public string? Password { get; set; }
+
+        // Các trường hệ thống có thể bỏ qua (tự động tạo)
+        // public int UserId { get; set; } // Không cần trong DTO đầu vào 
+        // public DateOnly? CreateDate { get; set; } // Tự động tạo khi lưu vào DB
+
+    }
+}
