1751d676970360fe3cdf5ac0f9dbb00ac6473730
Hieu Em <hieubmk2210@gmail.com>
2025-05-27 16:22:17 +0700

    Initial push HealthcareSystem backend

--- backend/HealthcareSystem.Application/DTOs/BlogDetailDTO.cs
+++ backend/HealthcareSystem.Application/DTOs/BlogDetailDTO.cs
@@ -3,9 +3,9 @@ namespace Application.DTOs
     public class GetBlogContentDTO
     {
         public int BlogID { get; set; }
-        public required string Title { get; set; }
-        public required string Content { get; set; }
-        public required string Topic { get; set; }
+        public required string Title { get; set; } // tiêu đề của bài blog đó 
+        public required string Content { get; set; }  // nội dung toàn bộ của bài blog đó 
+        public required string Topic { get; set; } // chủ đề 
         public DateTime PublishDate { get; set; }
         public required string ConsultantName { get; set; }
         public List<BlogImageDTO> Images { get; set; } = new();
