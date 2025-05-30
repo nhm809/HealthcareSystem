3d691eea2c6af30601208bfcb2cc98722b680e39
nhm809 <mynhse181827@fpt.edu.vn>
2025-05-31 00:55:17 +0700

    fixed

--- backend/HealthcareSystem.API/Program.cs
+++ backend/HealthcareSystem.API/Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Infrastructure.data;
+using Infrastructure.data;
 using Microsoft.EntityFrameworkCore;
 using Application.Interfaces;
 using Infrastructure.Services;
@@ -24,13 +23,6 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Healthcare API", Version = "v1" });
 });
 
-=======
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
->>>>>>> parent of 451843a (Update CreateDatabase.sql and InsertDB.sql)
 
 var app = builder.Build();
 
@@ -48,34 +40,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-<<<<<<< HEAD
 app.MapControllers(); // quan trọng! Định tuyến đến các Controller
 
 app.Run();
-=======
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
->>>>>>> parent of 451843a (Update CreateDatabase.sql and InsertDB.sql)
