29e78500322ab3523d0c98262f19c08a5365e8ea
nhm809 <mynhse181827@fpt.edu.vn>
2025-05-30 11:28:46 +0700

    Revert "Push code to HealthcareSystem"
    This reverts commit 8f535b4b36f3862219fadaf32fb5dd73366d64b8.

--- backend/HealthcareSystem.Application/DTOs/BlogDTO.cs
+++ backend/HealthcareSystem.Application/DTOs/BlogDTO.cs
@@ -1,12 +1,12 @@
 namespace Application.DTOs
 {
-    public class BlogDTO
+    public class GetBlogDTO
     {
         public int BlogID { get; set; }
         public required string Title { get; set; } // tiêu đề của bài blog 
         public required string Description { get; set; } // tóm tắt bài blog đó để người xem dễ biết bài blog đó có hữu ích với mình ko 
         public required string Topic { get; set; } // chủ để của bài blog 
-        public DateOnly PublishDate { get; set; }
+        public DateTime PublishDate { get; set; }
         public required string ConsultantName { get; set; }
         public required string ThumbnailImagePath { get; set; } // Ảnh ban cho bài content đó 
     }
--- backend/HealthcareSystem.Application/DTOs/BlogDetailDTO.cs
+++ backend/HealthcareSystem.Application/DTOs/BlogDetailDTO.cs
@@ -1,12 +1,12 @@
 namespace Application.DTOs
 {
-    public class BlogDetailDTO
+    public class GetBlogContentDTO
     {
         public int BlogID { get; set; }
         public required string Title { get; set; } // tiêu đề của bài blog đó 
         public required string Content { get; set; }  // nội dung toàn bộ của bài blog đó 
         public required string Topic { get; set; } // chủ đề 
-        public DateOnly PublishDate { get; set; }
+        public DateTime PublishDate { get; set; }
         public required string ConsultantName { get; set; }
         public List<BlogImageDTO> Images { get; set; } = new();
     }
@@ -16,7 +16,7 @@ namespace Application.DTOs
         public int ImageID { get; set; }
         public required string ImagePath { get; set; }
         public required string ImageCaption { get; set; }
-        public DateOnly PublishDate { get; set; }
+        public DateTime UploadDate { get; set; }
         public int OrderIndex { get; set; }
     }
 }
