5bc44089a8e6f0b2793faf5ad76c7e2452a65e3e
NguyenTrongTot <164918193+TrongtotDHFPT@users.noreply.github.com>
2025-06-10 22:30:53 +0700

    CRUD service-Dashboard Manager

--- backend/HealthcareSystem.Application/DTOs/ServiceDTO.cs
+++ backend/HealthcareSystem.Application/DTOs/ServiceDTO.cs
@@ -8,6 +8,20 @@ namespace Application.DTOs
 
         public string? Name { get; set; }
 
+        public string? Description { get; set; }
+        
         public decimal? Price { get; set; }
+    }
+    public class CreateServiceDTO
+    {
+        [Required(ErrorMessage = "Tên dịch vụ không được để trống")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Mô tả không được để trống")]
+        public string Description { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0")]
+        public decimal? Price { get; set; }
+
     }
 }
