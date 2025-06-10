b1800f2a173268a449067842c29d7ef6705c8899
nhm809 <mynhse181827@fpt.edu.vn>
2025-06-10 17:24:52 +0700

    Upgrade GoogleLoginService

--- backend/HealthcareSystem.Application/DTOs/GoolgeLoginDTO.cs
+++ backend/HealthcareSystem.Application/DTOs/GoolgeLoginDTO.cs
@@ -10,5 +10,9 @@ namespace Application.DTOs
         public string? Email { get; set; } // Email
         public bool? Email_verified { get; set; } // EmailVerified
         public string? Locale { get; set; } // Locale
+        public string? Token { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? ExpiresAcessToken { get; set; }
+        public DateTime? ExpiresRefreshToken { get; set; }
     }
 }
