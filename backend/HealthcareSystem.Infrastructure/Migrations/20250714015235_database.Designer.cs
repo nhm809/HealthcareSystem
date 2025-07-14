c7e815509a0d9761a4ace5607abba67e68a71dbd
tongngocanhtaik18 <taitnase181917@fpt.edu.vn>
2025-07-14 09:12:46 +0700

    fix

--- backend/HealthcareSystem.Infrastructure/Migrations/20250714015235_database.Designer.cs
+++ backend/HealthcareSystem.Infrastructure/Migrations/20250714015235_database.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthcareSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250713105341_database")]
+    [Migration("20250714015235_database")]
     partial class database
     {
         /// <inheritdoc />
