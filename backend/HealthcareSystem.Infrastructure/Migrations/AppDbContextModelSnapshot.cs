ea39682647a39e43a69b395bb22afb7482da5a74
NguyenTrongTot <164918193+TrongtotDHFPT@users.noreply.github.com>
2025-07-24 16:19:36 +0700

    fix database Appointment /TestServiceRecord/ Feedback / Invoice

--- backend/HealthcareSystem.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ backend/HealthcareSystem.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -4,7 +4,6 @@ using Infrastructure.data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,11 +11,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthcareSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250724072133_database")]
-    partial class database
+    partial class AppDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
